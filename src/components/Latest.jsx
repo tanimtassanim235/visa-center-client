0815ed969772ead6340f4c8a693e25ce01821803
Tanim Tassanim <tanimtassanim235@gmail.com>
2024-12-12 20:11:36 +0600

    see all visa added

--- src/components/Latest.jsx
+++ src/components/Latest.jsx
@@ -1,14 +1,20 @@
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import VisaCard from "./VisaCard";
 
 const Latest = () => {
     const visas = useLoaderData();
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-            {
-                visas.map(visaes => <VisaCard key={visaes._id} visaes={visaes}></VisaCard>)
-            }
+        <div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+                {
+                    visas.map(visaes => <VisaCard key={visaes._id} visaes={visaes}></VisaCard>)
+                }
+            </div>
+            <div className="text-center">
+                <Link className="btn btn-error my-8" to="/allvisas">See all visas</Link>
+            </div>
         </div>
+
     );
 };
 
